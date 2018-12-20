cdb5c09f4f9d85ef481321f6c64d8638dde44108
Edson <edsaraujoc@gmail.com>
2018-12-20 15:18:07 -0200

    Implementacao

--- ordenacao.cpp
+++ ordenacao.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
-	cout << "Ok" << endl;
+	int lista[5];
+	lista[0] = 10;
+	lista[1] = 30;
+	lista[2] = 32;
+	lista[3] = 21;
+	lista[4] = 31;
+
+	int menorValor = 0;
+	
+	for(int i = 0; i <= 4; i++) {
+		if(lista[i] < lista[menorValor]) {
+			menorValor = i;
+		}
+	}
+	
+	cout << "O menor valor " << lista[menorValor] << endl;
 	return 0;
 }
